2ec8bde941808b5e58bbbcd1b9c7f0ebc069beea
José Cleyton <cleytoncomp@gmail.com>
2020-04-22 16:29:39 -0300

    Criando classe de filtro CORS

--- src/main/java/com/br/apiDivinaProvidencia/security/WebSecurityConfig.java
+++ src/main/java/com/br/apiDivinaProvidencia/security/WebSecurityConfig.java
@@ -12,12 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.br.apiDivinaProvidencia.filter.CorsFilter;
 import com.br.apiDivinaProvidencia.filter.JwtFilter;
 import com.br.apiDivinaProvidencia.services.JwtService;
 import com.br.apiDivinaProvidencia.services.impl.UserServiceImpl;
@@ -47,23 +49,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-		http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable().authorizeRequests()
-				.antMatchers("/orders/**").hasRole("ADMIN").antMatchers("/products/**").hasRole("ADMIN")
-				.antMatchers("/cashier/**").hasRole("ADMIN").antMatchers("/stock/**").hasRole("ADMIN")
+//cors().configurationSource(corsConfigurationSource()).and().
+		http.csrf().disable().authorizeRequests().antMatchers("/orders/**").hasRole("ADMIN").antMatchers("/products/**")
+				.hasRole("ADMIN").antMatchers("/cashier/**").hasRole("ADMIN").antMatchers("/stock/**").hasRole("ADMIN")
 				.antMatchers("/client/**").hasRole("ADMIN").antMatchers("/reportCashier/**").hasRole("ADMIN")
 				.antMatchers(HttpMethod.POST, "/user/**").permitAll().anyRequest().authenticated().and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+				.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
 
 	}
 
-	@Bean
+	// @Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedHeaders(
-				Arrays.asList("Access-Control-Allow-Origin","Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", "Accept"));
+		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+				"Origin", "X-Requested-With", "Content-Type", "Accept"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
