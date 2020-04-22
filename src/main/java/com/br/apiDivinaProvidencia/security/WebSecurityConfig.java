fdadb2524b6d2a8683c2da21aa78fab0f9819c6b
José Cleyton <cleytoncomp@gmail.com>
2020-04-22 14:51:06 -0300

    Testando nova configuração de CORS

--- src/main/java/com/br/apiDivinaProvidencia/security/WebSecurityConfig.java
+++ src/main/java/com/br/apiDivinaProvidencia/security/WebSecurityConfig.java
@@ -48,26 +48,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.cors().configurationSource(corsConfigurationSource())
-				.and().csrf().disable()
-				.authorizeRequests().antMatchers("/orders/**").hasRole("ADMIN")
-				.antMatchers("/products/**").hasRole("ADMIN").antMatchers("/cashier/**").hasRole("ADMIN")
-				.antMatchers("/stock/**").hasRole("ADMIN").antMatchers("/client/**").hasRole("ADMIN")
-				.antMatchers("/reportCashier/**").hasRole("ADMIN").antMatchers(HttpMethod.POST, "/user/**").permitAll()
-				.anyRequest().authenticated().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable().authorizeRequests()
+				.antMatchers("/orders/**").hasRole("ADMIN").antMatchers("/products/**").hasRole("ADMIN")
+				.antMatchers("/cashier/**").hasRole("ADMIN").antMatchers("/stock/**").hasRole("ADMIN")
+				.antMatchers("/client/**").hasRole("ADMIN").antMatchers("/reportCashier/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/user/**").permitAll().anyRequest().authenticated().and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
 	}
-	
+
 	@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-   }
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedHeaders(
+				Arrays.asList("Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", "Accept"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
